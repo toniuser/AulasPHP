d66431a6e5527e4786c5e4aa8e2ba12d81ee1bc4
Toni Vicente <toni15x@hotmail.com>
2019-02-23 13:38:16 -0300

    Aula de comentarios e procedencia

---
+++ Aula - basico 1/precedencia.php
@@ -0,0 +1,12 @@
+<?php
+
+$soma = 5+ 3 * 8;
+// Procedencia realiza a leitura da esquerda ------> para direita, porem interpreta respeitando regas como matematicas ou aritimeticas.
+
+echo $soma;
+echo "<br>";
+
+
+$soma2 = 15 + (15 - 2 * 8) * 8;
+
+echo $soma2;
